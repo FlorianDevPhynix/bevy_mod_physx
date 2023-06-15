59df676f0baed85cb21d1d510362f8c51ef165d7
Florian <florian.stemmler@outlook.de>
2023-06-15 20:56:28 +0200

    added PhysXSettings and update_gravity system
    gravity and thread count can be set through resource, changes to gravity on runtime update the simulation

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,9 @@ impl Plugin for PhysXPlugin {
         app 
             //register
             .register_type::<PxPlane>()
+            // settings
+            .register_type::<PhysXSettings>()
+            .init_resource::<PhysXSettings>()
 
             //setup physx
             .add_startup_system(setup_physx)
@@ -63,6 +66,7 @@ impl Plugin for PhysXPlugin {
             .add_systems(( //after flush
                 new_articulation_joint,
                 new_collider,
+                update_gravity,
                 // update_changed_transform,
                 update_mass_properties_system,
                 update_damping_system,
@@ -215,10 +219,54 @@ fn px_step_simulation(
     }
 }
 
+#[derive(Resource, Reflect)]
+pub struct PhysXSettings {
+    pub gravity: Vec3,
+    /// can't be changed after PhysX initialization
+    pub thread_count: u32,
+}
 
+impl Default for PhysXSettings {
+    fn default() -> Self {
+        Self {
+            gravity: Vec3::new( 0.0, -9.81, 0.0 ),
+            thread_count: std::thread::available_parallelism().unwrap().get() as u32,
+        }
+    }
+}
+
+// get starting value from world so this can be used for detecting changes
+/* impl FromWorld for PhysXSettings {
+    fn from_world(world: &mut World) -> Self {
+        if let Some(value) = world.get_resource::<Self>() {
+            *value.to_owned()
+        } else {
+            Self {
+                gravity: Vec3::new( 0.0, -9.81, 0.0 ),
+                thread_count: std::thread::available_parallelism().unwrap().get() as u32,
+            }
+        }
+    }
+} */
+
+// 15.06.2023: not perfect change detection, runs once on first call
+// setting gravity once on first FixedUpdate is useless, does not seem like a big problem;
+// perfect detection possibly with Local resource that holds old value,
+// needs default value, implementing FromWorld to assign the resource value before the first execution
+fn update_gravity(
+    mut physx: ResMut<PhysX>,
+    settings: Res<PhysXSettings>,
+    //mut old_setting: Local<PhysXSettings>
+) {
+
+    if settings.is_changed() {
+        physx.scene.set_gravity( settings.gravity.x, settings.gravity.y, settings.gravity.z );
+    }
+}
 
 fn setup_physx(   
     mut commands: Commands,
+    settings: Res<PhysXSettings>
 ){
 
     let mut foundation: PhysicsFoundation<physx::foundation::DefaultAllocator, PxShape>;
@@ -236,13 +284,12 @@ fn setup_physx(
         None => Default::default(),
     };
 
-
     let scene: Owner<PxScene> = foundation
         .create(SceneDescriptor {
-            gravity: PxVec3::new(0.0, -9.81, 0.0),
+            gravity: vec3_to_physx( settings.gravity ),
             on_advance: Some(OnAdvance),
             simulation_filter_shader: FilterShaderDescriptor::Custom(costum_filter_shader),
-            thread_count: std::thread::available_parallelism().unwrap().get() as u32,
+            thread_count: settings.thread_count,
             ..SceneDescriptor::new(()) 
         })
         .unwrap();
